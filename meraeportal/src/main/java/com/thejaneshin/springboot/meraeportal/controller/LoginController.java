da8ae5938500b782c984610f9cffeff310cc33c6
Jane Shin <thejaneshin@gmail.com>
2019-03-08 12:44:40 -0800

    Implemented the UI for login page

---
+++ meraeportal/src/main/java/com/thejaneshin/springboot/meraeportal/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.thejaneshin.springboot.meraeportal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	@GetMapping("/login")
+	public String showMyLoginPage() {
+		return "login";
+	}
+	
+	@GetMapping("/access-denied")
+	public String showAccessDenied() {
+		return "access-denied";	
+	}
+}
