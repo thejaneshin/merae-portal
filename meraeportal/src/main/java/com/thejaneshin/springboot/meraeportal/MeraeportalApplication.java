2888cbcfbd9c8166c39e3850a04a8618eb2a507a
Jane Shin <thejaneshin@gmail.com>
2019-03-08 11:17:10 -0800

    First commit

---
+++ meraeportal/src/main/java/com/thejaneshin/springboot/meraeportal/MeraeportalApplication.java
@@ -0,0 +1,13 @@
+package com.thejaneshin.springboot.meraeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeraeportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeraeportalApplication.class, args);
+	}
+
+}
